7cb1b14765ef15dc6cc903116faf25bfd95dd949
yutaka <p7dea87c7e4n7j@gmail.com>
2020-02-05 22:34:35 +0900

    implement readdcd until body block

--- src/component.jl
+++ src/component.jl
@@ -1,14 +1,25 @@
 struct Atom
-    coordinate::Vector{Float64}
+
+    coordinate::Vector{Float64} # [x, y, z]
     resname::String
     resid::Int64
     atomname::String
     atomid::Int64
     mass::Float64
+
+    Atom(coordinate) = new(coordinate)
 end
 
-struct Frame
-    atoms::Vector{Atom}
-    natom::Int64
+struct Trajectory
+
+    coordinates::Matrix{Atom}
+    # The column of coordinates matrix means one snapshot.
+    # The row of coordinates matrix means time series of one atom.
+    # [ a d g j
+    #   b e h k
+    #   c f i l ]
+    # In this case, one snapshot correspond to [a b c]
     nframe::Int64
+
+    Trajectory(coordinates) = new(coordinates, size(coordinates, 2))
 end
--- src/fileio.jl
+++ src/fileio.jl
@@ -1,7 +1,7 @@
 function readdcd(filename::String)
     open(filename, "r") do io
-        # read first block
-        block_size_1 = read(io, Int32)
+        # read header first block
+        skip(io, 4) # skip block size part
         header_sig = Array{Char, 1}(undef, 4)
         for i in 1:4
             header_sig[i] = read(io, Char)
@@ -17,15 +17,57 @@ function readdcd(filename::String)
         header_null_9 = Array{Int32, 1}(undef, 9)
         read!(io, header_null_9)
         version   = read(io, Int32)
-        block_size_2 = read(io, Int32)
-        println("header sig ", header_sig)
-        println("total frame ", total_frame)
-        println("first step ", first_step)
-        println("nstep save ", nstep_save)
-        println("total step ", total_step)
-        println("total unit ", total_unit)
-        println("time_step ", time_step)
-        println("version ", version)
-        println("block size 2 ", block_size_2)
+        skip(io, 4) # skip block size part
+#        println("header sig ", header_sig)
+#        println("total frame ", total_frame)
+#        println("first step ", first_step)
+#        println("nstep save ", nstep_save)
+#        println("total step ", total_step)
+#        println("total unit ", total_unit)
+#        println("time_step ", time_step)
+#        println("version ", version)
+
+        # read header second block
+        skip(io, 4)
+        number_of_lines    = read(io, Int32)
+        title = []
+        for i in 1:number_of_lines
+            line = Array{Char, 1}(undef, 80)
+            for i in 1:80
+                line[i] = read(io, Char)
+            end
+            push!(title, String(line))
+        end
+        skip(io, 4) # skip block size part
+#        println("title \n", title)
+
+        # read header third block
+        skip(io, 4) # skip block size part
+        number_of_atom     = read(io, Int32)
+        skip(io, 4) # skip block size part
+#       println("number_of_atom ", number_of_atom)
+
+        coordinates_time_series = Matrix{Atom}(undef, number_of_atom, total_frame)
+        for frame_idx in 1:total_frame
+            # read x coordinates
+            skip(io, 4) # skip block size part
+            x_coords = Array{Float32, 1}(undef, number_of_atom)
+            read!(io, x_coords)
+            skip(io, 4) # skip block size part
+
+            # read y coorinates
+            skip(io, 4) # skip block size part
+            y_coords = Array{Float32, 1}(undef, number_of_atom)
+            skip(io, 4) # skip block size part
+
+            # read z coordinates
+            skip(io, 4) # skip block size part
+            z_coords = Array{Float32, 1}(undef ,number_of_atom)
+            skip(io, 4) # skip block size part
+
+            atoms = map(xyz -> Atom(xyz), eachrow(hcat(x_coords, y_coords, z_coords)))
+            all_coordinates[:, frame_idx] = atoms
+        end
+        Trajectory(trj)
     end
 end
