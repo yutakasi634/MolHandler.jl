04a8abeb7270d1f1962e419559cbc137cf5aafa4
yutaka <p7dea87c7e4n7j@gmail.com>
2020-03-10 16:10:15 +0900

    feat: export distance method for Coordinate struct

--- src/MolHandler.jl
+++ src/MolHandler.jl
@@ -1,6 +1,6 @@
 module MolHandler
 
-export Coordinate, norm
+export Coordinate, norm, distance
 export Atom, Attribute, Frame, Trajectory
 export readdcd, readpdb
 export get_frame, get_atom, clip_trajectory, center_of_mass
