2d8a47683767ec79c1ae6a3452acf0756e299b43
yutaka <p7dea87c7e4n7j@gmail.com>
2020-10-15 21:34:01 +0900

    feat: export residue_mass method

--- src/MolHandler.jl
+++ src/MolHandler.jl
@@ -8,7 +8,7 @@ export get_frame, get_atom, clip_trajectory, center_of_mass,
        contact_bool_matrix, contact_bool_matrix_parallel,
        contact_probability_matrix, contact_probability_matrix_parallel,
        radius_of_gyration,
-       atom_mass
+       atom_mass, residue_mass
 
 # codes
 include("variables.jl")
